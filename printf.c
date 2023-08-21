cad6d51718e56916e053b28908fbe28967e150a9
Richie911 <oohrichmond.k@gmail.com>
2023-08-21 14:52:02 -0700

    update printf

--- printf.c
+++ printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count = 0;
 	const char *ch;
+	char placeholders[] = {'c', 'd', 'f', 'i', 'o', 'u', 's', '%'};
+	int j;
 
 	if (format == NULL)
 		return (-1);
@@ -20,15 +22,21 @@ int _printf(const char *format, ...)
 		if (*ch == '%')
 		{
 			ch++;
-			if (*ch == 'c')
-				handle_char(args);
-			else if (*ch == 's')
-				count += handle_string(args);
-			else if (*ch == '%')
-				handle_percent();
-			else if (*ch == 'i' || *ch == 'd')
-				handle_int(args);
-			count++;
+			for (j = 0; j <8; j++)
+			{
+				if (*ch == placeholders[j])
+				{
+					if (*ch == 'c')
+						handle_char(args);
+					else if (*ch == 's')
+						count += handle_string(args);
+					else if (*ch == '%')
+						handle_percent();
+					else if (*ch == 'i' || *ch == 'd')
+						handle_int(args);
+					count++;
+				}
+			}
 		}
 		else
 		{
