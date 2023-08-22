f7e476d16341897a3df81ad7e6a16a8bc18e66d8
Richie911 <oohrichmond.k@gmail.com>
2023-08-22 09:48:25 -0700

    printf updated

--- printf.c
+++ printf.c
@@ -17,6 +17,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (ch = format; *ch != '\0'; ch++)
 	{
+		if (*ch == '%' && (*(ch + 1)) == '\0')
+			return (-1);
 		if (*ch == '%' && (*(ch + 1)) != '\0')
 		{
 			ch++;
@@ -38,8 +40,6 @@ int _printf(const char *format, ...)
 			}
 			count++;
 		}
-		else if (*ch == '%' && (*(ch + 1)) == '\0')
-			count = -1;
 		else
 		{
 			write(1, ch, 1);
